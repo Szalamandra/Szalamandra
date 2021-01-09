087d329ce6ca344b7d85c42941cf047a880a4797
Szalamandra <Szalamandra@users.noreply.github.com>
2021-01-09 23:01:46 +0100

    mar majdnem jo  amenu

---
+++ wp-content/cache/autoptimize/js/autoptimize_e5862fadd53b34db8facd671b2e730c5.js
@@ -0,0 +1,11 @@
+(function($){wp.customize('blogname',function(value){value.bind(function(to){$('.site-title a').text(to);});});wp.customize('blogdescription',function(value){value.bind(function(to){$('.site-description').text(to);});});wp.customize('header_textcolor',function(value){value.bind(function(to){if('blank'===to){$('.site-title, .site-description').css({clip:'rect(1px, 1px, 1px, 1px)',position:'absolute',});}else{$('.site-title, .site-description').css({clip:'auto',position:'relative',});$('.site-title a, .site-description').css({color:to,});}});});}(jQuery));
+(function(){const siteNavigation=document.getElementById('site-navigation');if(!siteNavigation){return;}
+const button=siteNavigation.getElementsByTagName('button')[0];if('undefined'===typeof button){return;}
+const menu=siteNavigation.getElementsByTagName('ul')[0];if('undefined'===typeof menu){button.style.display='none';return;}
+if(!menu.classList.contains('nav-menu')){menu.classList.add('nav-menu');console.log("benne vagyok");}
+button.addEventListener('click',function(){siteNavigation.classList.toggle('toggled');if(button.getAttribute('aria-expanded')==='true'){button.setAttribute('aria-expanded','false');}else{button.setAttribute('aria-expanded','true');}});document.addEventListener('click',function(event){const isClickInside=siteNavigation.contains(event.target);if(!isClickInside){siteNavigation.classList.remove('toggled');button.setAttribute('aria-expanded','false');}});const links=menu.getElementsByTagName('a');const linksWithChildren=menu.querySelectorAll('.menu-item-has-children > a, .page_item_has_children > a');for(const link of links){link.addEventListener('focus',toggleFocus,true);link.addEventListener('blur',toggleFocus,true);}
+for(const link of linksWithChildren){link.addEventListener('touchstart',toggleFocus,false);}
+function toggleFocus(){if(event.type==='focus'||event.type==='blur'){let self=this;while(!self.classList.contains('nav-menu')){if('li'===self.tagName.toLowerCase()){self.classList.toggle('focus');}
+self=self.parentNode;}}
+if(event.type==='touchstart'){const menuItem=this.parentNode;event.preventDefault();for(const link of menuItem.parentNode.children){if(menuItem!==link){link.classList.remove('focus');}}
+menuItem.classList.toggle('focus');}}}());
